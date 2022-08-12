2c7a7e09071eed2e49ccad7b7fe21322892c40d1
Oleg <mr.lost@rambler.ru>
2022-08-12 14:16:23 +0300

    End

--- src/components/itemList/itemList.js
+++ src/components/itemList/itemList.js
@@ -1,56 +1,68 @@
 import React, { Component } from 'react';
 import Spiner from '../spinner';
 import { Link } from 'react-router-dom';
-
 import './itemList.css';
-export default class ItemList extends Component {
 
-    state = {
-        itemList: null,
+const ItemList = (props) => {
 
-    }
-
-    componentDidMount() {
-        const { getData } = this.props;
-
-        getData()
-            .then((itemList) => {
-                this.setState({
-                    itemList
-                })
-            })
-    }
-
-    renderItems(arr) {
+    const renderItems = (arr) => {
         return arr.map((elem, i) => {
             const { id } = elem;
-            const label = this.props.renderItem(elem)
+            const label = props.renderItem(elem)
 
             return (
                 <Link to={`${id || i + 1}`}
                     key={id || i + 1}
                     className="list-group-item"
-                    onClick={() => this.props.onItemSelected(id || i + 1)}>
+                    onClick={() => props.onItemSelected(id || i + 1)}>
                     {label}
                 </Link>
             )
         })
     }
 
-    render() {
-        const { itemList } = this.state;
-        if (!itemList) {
-            return <Spiner color='blue' size='30' />
+
+    const { data } = props
+    const items = renderItems(data)
+
+    return (
+        <div className='item-details rounded'>
+            <ul className="item-list list-group" >
+                {items}
+            </ul>
+        </div>
+    );
+};
+
+const withData = (View) => {
+    return class extends Component {
+
+        state = {
+            data: null,
+
         }
 
-        const items = this.renderItems(itemList)
+        componentDidMount() {
+            const { getData } = this.props;
 
-        return (
-            <div className='item-details rounded'>
-                <ul className="item-list list-group" >
-                    {items}
-                </ul>
-            </div>
-        );
+            getData()
+                .then((data) => {
+                    this.setState({
+                        data
+                    })
+                })
+        }
+
+        render() {
+            const { data } = this.state;
+
+            if (!data) {
+                return <Spiner color='blue' size='30' />
+            }
+
+            return <View {...this.props} data={data} />
+        }
     }
 }
+
+export default withData(ItemList);
